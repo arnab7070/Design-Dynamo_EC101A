789e2b22d7c652a1dba9776e931826a24d082be2
Arnab Nandi <arnabnandi7074@gmail.com>
2024-03-03 23:50:04 +0530

    Pricing Card changed

--- yumYard/src/Components/PricingCard.jsx
+++ yumYard/src/Components/PricingCard.jsx
@@ -51,7 +51,7 @@ const PricingCard = () => {
                                     </svg>
                                 </span>Discover new restaurants.
                             </p>
-                            <button class="flex items-center mt-auto text-white bg-orange-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-orange-500 rounded">Buy Now
+                            <button class="flex items-center mt-auto text-white bg-orange-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-orange-500 rounded">Start @ zero cost now
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
