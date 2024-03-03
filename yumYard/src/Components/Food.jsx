1ced741d4dfb15d61eecd81ae262b62924184974
BIBHAB22 <bibhabindia@gmail.com>
2024-03-03 13:26:07 +0530

    Food Section Scrooling

--- yumYard/src/Components/Food.jsx
+++ yumYard/src/Components/Food.jsx
@@ -3,9 +3,8 @@ import React from "react";
 const Food = () => {
   return (
     <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto flex items-center ">
-        <div className="flex items-center justify-between w-full max-w-screen-lg overflow-x-auto">
-          {/* Left arrow button */}
+      <div className="container px-5 py-24 mx-auto flex items-center">
+        <div className="flex items-center justify-between w-full overflow-x-auto">
           <button
             className="bg-gray-200 rounded-full p-2"
             onClick={() => {
@@ -14,20 +13,16 @@ const Food = () => {
           >
             {"<"}
           </button>
-
-          {/* Items container */}
           <div
             id="food-container"
             className="flex space-x-4 flex-no-wrap overflow-x-auto"
           >
-            {/* Your item cards */}
             {[...Array(8)].map((_, index) => (
               <div
                 key={index}
                 className="lg:w-1/4 md:w-1/2 p-4"
                 style={{ minWidth: "250px" }}
               >
-                {/* Your item card */}
                 <div className="h-64 bg-gray-200">Item {index + 1}</div>
               </div>
             ))}
