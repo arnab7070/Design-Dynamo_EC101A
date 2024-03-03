13b71f49cdc303a53c626a713f577259ab4bb5cf
Arnab Nandi <arnabnandi7074@gmail.com>
2024-03-03 10:56:05 +0530

    contact form added

---
+++ yumYard/src/Components/ContactForm.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+const ContactForm = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://static-bundles.visme.co/forms/vismeforms-embed.js';
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div className="visme_d" data-title="Contact Form" data-url="dm0xvwq1-contact-form" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="37569"></div>
+    );
+};
+
+export default ContactForm;
--- yumYard/src/Pages/Home.jsx
+++ yumYard/src/Pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Navbar } from '../Components/Navbar'
 import { HeroSection } from '../Components/HeroSection'
-
+import ContactForm from "../Components/ContactForm"
 export const Home = () => {
     return (
         <div className='home'>
             <div className="main">
                 <Navbar/>
                 <HeroSection/>
+                <ContactForm />
             </div>
         </div>
     )
