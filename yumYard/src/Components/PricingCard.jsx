22eab4d2b299b199de26aaff6a290a5365aaa626
Arnab Nandi <arnabnandi7074@gmail.com>
2024-03-03 20:50:50 +0530

    final animation added

--- yumYard/src/Components/PricingCard.jsx
+++ yumYard/src/Components/PricingCard.jsx
@@ -18,7 +18,7 @@ const PricingCard = () => {
         <section class="text-gray-600 body-font overflow-hidden">
             <div class="container px-5 py-24 mx-auto">
                 <div data-aos="zoom-in-down" class="flex flex-col text-center w-full mb-20">
-                    <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Unlock your perfect food experience</h1>
+                    <h1 class="sm:text-4xl text-3xl capitalize font-medium title-font mb-2 text-orange-500">Unlock your perfect food experience</h1>
                     <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Choose the plan that fits your cravings.</p>
                     {/* <div class="flex mx-auto border-2 border-orange-500 rounded overflow-hidden mt-6">
                         <button class="py-1 px-4 bg-orange-500 text-white focus:outline-none">Monthly</button>
