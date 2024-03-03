c91d1e7fcdd2ea82e3afc4e2d96dd0ab4e5bfccd
BIBHAB22 <bibhabindia@gmail.com>
2024-03-03 12:39:23 +0530

    Food section added

--- yumYard/src/Pages/Home.jsx
+++ yumYard/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar } from '../Components/Navbar'
 import { HeroSection } from '../Components/HeroSection'
+import Food from "../Components/Food"
 import ContactForm from "../Components/ContactForm"
 import FooterComponent from '../Components/Footer'
 import Services from '../Components/Services'
@@ -10,6 +11,7 @@ export const Home = () => {
             <div className="main">
                 <Navbar/>
                 <HeroSection/>
+                <Food/>
                 <Services />
                 <ContactForm />
                 <FooterComponent/>
