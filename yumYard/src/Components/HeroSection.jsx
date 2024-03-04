31934156894121f9df08837cd86176d5eea7e66a
Supra707 <supratikde2@gmail.com>
2024-03-04 11:01:06 +0530

    fixes

--- yumYard/src/Components/HeroSection.jsx
+++ yumYard/src/Components/HeroSection.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Dropdown } from 'flowbite-react';
-import Customer1 from "../Images/customers/customer-1.jpg"
-import Customer2 from "../Images/customers/customer-2.jpg"
-import Customer3 from "../Images/customers/customer-3.jpg"
-import Customer4 from "../Images/customers/customer-4.jpg"
-import Customer5 from "../Images/customers/customer-5.jpg"
+import { Dropdown } from "flowbite-react";
+import Customer1 from "../Images/customers/customer-1.jpg";
+import Customer2 from "../Images/customers/customer-2.jpg";
+import Customer3 from "../Images/customers/customer-3.jpg";
+import Customer4 from "../Images/customers/customer-4.jpg";
+import Customer5 from "../Images/customers/customer-5.jpg";
 import arrow from "../Images/svg/arrow-down-short.svg";
 import Lottie from "lottie-react";
-import burger from "../assets/burger.json"
+import burger from "../assets/burger.json";
 export const HeroSection = () => {
   return (
     <div class="relative w-full">
       <div class="relative bg-gradient-to-r from-amber-200 to-yellow-300">
         <div class="container m-auto px-6 pt-23 md:px-12 lg:pt-[1.5rem] lg:px-9 lg:pl-20">
-          <div class="flex items-center flex-wrap px-2 md:px-0">
+          <div class="flex gap-x-24 items-center flex-wrap px-2 md:px-0">
             <div class="relative lg:w-6/12 lg:py-24 xl:py-32">
               <h1 class="font-bold text-4xl text-yellow-900 md:text-5xl">
                 Savor Every Bite: Delightful Dishes Delivered to Your Doorstep
@@ -21,10 +21,18 @@ export const HeroSection = () => {
               <form action="" class="w-full mt-12">
                 <div class="relative gap-x-3 flex p-1 rounded-full bg-white border border-yellow-200 shadow-md md:p-2">
                   <div className="border border-gray-500 pr-3 rounded-full ">
-                    <Dropdown className="bg-transparent backdrop-blur-md" label="Category" dismissOnClick={true} renderTrigger={() => <span><div className="inline-flex cursor-pointer rounded-full p-3 md:p-4">
-                      Category <img src={arrow} alt="arrow" />
-                    </div>
-                    </span>}>
+                    <Dropdown
+                      className="bg-transparent backdrop-blur-md"
+                      label="Category"
+                      dismissOnClick={true}
+                      renderTrigger={() => (
+                        <span>
+                          <div className="inline-flex cursor-pointer rounded-full p-3 md:p-4">
+                            Category <img src={arrow} alt="arrow" />
+                          </div>
+                        </span>
+                      )}
+                    >
                       <Dropdown.Item>FastFood</Dropdown.Item>
                       <Dropdown.Item>Restaurant</Dropdown.Item>
                       <Dropdown.Item>Take Out</Dropdown.Item>
@@ -61,7 +69,7 @@ export const HeroSection = () => {
                 </a>{" "}
                 tantalizing menu crafted to perfection.
               </p>
-              <div class=" mt-8 px-4 py-3 flex flex-col sm:flex-row gap-x-8 items-center rounded-btn border-4 border-[rgba(254,101,54,0.44)] glassEffect rounded-full ">
+              <div class=" bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10   mt-8 px-4 py-3 flex flex-col sm:flex-row gap-x-8 items-center rounded-btn   glassEffect  ">
                 <div class="flex flex-row items-center justify-center">
                   <a
                     target="_blank"
@@ -76,9 +84,7 @@ export const HeroSection = () => {
                       decoding="async"
                       data-nimg="1"
                       class="object-cover !m-0 !p-0 object-top rounded-full w-10 h-10 sm:h-12 sm:w-12 border-2   group-hover:scale-105 group-hover:z-30 border-black relative transition duration-500 undefined undefined"
-
                       src={Customer1}
-
                     />
                   </a>
                   <a
@@ -94,9 +100,7 @@ export const HeroSection = () => {
                       decoding="async"
                       data-nimg="1"
                       class="object-cover !m-0 !p-0 object-top rounded-full w-10 h-10 sm:h-12 sm:w-12 border-2   group-hover:scale-105 group-hover:z-30 border-black relative transition duration-500 undefined undefined"
-
                       src={Customer2}
-
                     />
                   </a>
                   <a
@@ -112,9 +116,7 @@ export const HeroSection = () => {
                       decoding="async"
                       data-nimg="1"
                       class="object-cover !m-0 !p-0 object-top rounded-full w-10 h-10 sm:h-12 sm:w-12 border-2   group-hover:scale-105 group-hover:z-30 border-black relative transition duration-500 undefined undefined"
-
                       src={Customer3}
-
                     />
                   </a>
                   <a
@@ -130,9 +132,7 @@ export const HeroSection = () => {
                       decoding="async"
                       data-nimg="1"
                       class="object-cover !m-0 !p-0 object-top rounded-full w-10 h-10 sm:h-12 sm:w-12 border-2   group-hover:scale-105 group-hover:z-30 border-black relative transition duration-500 undefined undefined"
-
                       src={Customer4}
-
                     />
                   </a>
                   <a
@@ -148,25 +148,20 @@ export const HeroSection = () => {
                       decoding="async"
                       data-nimg="1"
                       class="object-cover !m-0 !p-0 object-top rounded-full w-10 h-10 sm:h-12 sm:w-12 border-2   group-hover:scale-105 group-hover:z-30 border-black relative transition duration-500 undefined undefined"
-
                       src={Customer5}
-
                     />
                   </a>
                 </div>
-                <p class=" flex justify-between gap-5 items-center  text-base font-medium text-black">
-                  <span className="font-extarabold text-4xl text-yellow-400">
-                    60,000+ </span>  <p className="text-2xl font-bold">Happy Customers</p>
-                </p>
-
-
+                <p class="delivered-text">
+                <span>250,000+</span> meals delivered last year!
+              </p>
               </div>
             </div>
 
-            <div class="ml-auto -mb-24 lg:-mb-56 lg:w-6/12">
+             
+              {/* Adjust width and height as needed */}
               <Lottie animationData={burger} />
-
-            </div>
+        
           </div>
         </div>
       </div>
