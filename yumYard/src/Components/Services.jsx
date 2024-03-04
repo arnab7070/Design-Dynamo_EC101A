76d98be965c986babc4eb6adb9c6a26f8bb946f1
Arnab Nandi <arnabnandi7074@gmail.com>
2024-03-04 15:26:49 +0530

    services component added

--- yumYard/src/Components/Services.jsx
+++ yumYard/src/Components/Services.jsx
@@ -1,67 +1,71 @@
-import React from 'react'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect } from 'react'
-const Services = () => {
+import React, { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { FcBinoculars, FcInTransit, FcSms, FcAssistant } from 'react-icons/fc';
+
+const serviceData = [
+    {
+        title: "Wide Restaurant Selection",
+        description: "Find a diverse range of restaurants to different cuisines, budgets, and dietary preferences (gluten-free, vegan, etc.).",
+        icon: FcBinoculars,
+        color: "orange",
+    },
+    {
+        title: "Seamless Delivery Management",
+        description: "Track your order's progress, from preparation to delivery, and receive notifications at each stage.",
+        icon: FcInTransit,
+        color: "pink",
+    },
+    {
+        title: "Convenient Food Ordering",
+        description: "Order your favorite dishes directly through our app or website with real-time order tracking.",
+        icon: FcAssistant,
+        color: "red",
+    },
+    {
+        title: "Community Reviews & Ratings",
+        description: "Read and contribute genuine reviews to help others discover great food. Enjoy the service now!",
+        icon: FcSms,
+        color: "green",
+    }
+];
+
+const ServiceItem = ({ title, description, icon: Icon, color }) => {
     useEffect(() => {
-        Aos.init(
-            {
-                offset: 300,
-                duration: 400,
-                easing: 'ease-in',
-                delay: 200,
-            }
-        );
-    }, [])
+        Aos.init({
+            duration: 400,
+            easing: 'ease-in',
+            offset: 50,
+        });
+    }, []);
+
     return (
-        <section class="text-gray-600 body-font">
-            <div class="container px-5 py-24 mx-auto">
-                <div data-aos="fade-up" class="flex flex-col text-center w-full mb-20">
-                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-orange-500">Explore, Order, and Discover Delicious Food</h1>
-                    <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Your one-stop shop for all your food needs.</p>
+        <div data-aos="fade-down" className={`relative xl:w-1/4 lg:w-1/2 md:w-full px-6 py-4 border rounded-lg shadow-md shadow-orange-300 bg-${color}-100 hover:shadow-lg hover:shadow-orange-400 transition duration-300`}>
+            {Icon && <Icon className="absolute top-0 left-0 transform -translate-x-1/3 -translate-y-1/2 h-12 w-12" />}
+            <h2 data-aos="fade-right" className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">{title}</h2>
+            <p data-aos="fade-right" className="leading-relaxed text-base text-gray-700 mb-4">{description}</p>
+            <button data-aos="fade-down" className={`bg-${color}-500 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out`}>Learn More</button>
+        </div>
+    );
+
+};
+
+const Services = () => {
+    return (
+        <section id='services' className="text-gray-800 body-font">
+            <div className="container px-5 py-24 mx-auto ">
+                <div className="flex flex-col text-center w-full mb-20">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-orange-500">Explore, Order, and Discover Delicious Food</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Your one-stop shop for all your food needs.</p>
                 </div>
-                <div class="flex flex-wrap">
-                    <div data-aos="zoom-in" class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Wide Restaurant Selection</h2>
-                        <p class="leading-relaxed text-base mb-4">Find a diverse range of restaurants catering to different cuisines, budgets, and dietary preferences (gluten-free, vegan, etc.).</p>
-                        <a class="text-orange-500 inline-flex items-center">Learn More
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                <path d="M5 12h14M12 5l7 7-7 7"></path>
-                            </svg>
-                        </a>
-                    </div>
-                    <div data-aos="zoom-in" class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Convenient Food Ordering</h2>
-                        <p class="leading-relaxed text-base mb-4">Order your favorite dishes directly through our app or website with real-time order tracking.</p>
-                        <a class="text-orange-500 inline-flex items-center">Learn More
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                <path d="M5 12h14M12 5l7 7-7 7"></path>
-                            </svg>
-                        </a>
-                    </div>
-                    <div data-aos="zoom-in" class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Seamless Delivery Management</h2>
-                        <p class="leading-relaxed text-base mb-4">Track your order's progress, from preparation to delivery, and receive notifications at each stage.</p>
-                        <a class="text-orange-500 inline-flex items-center">Learn More
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                <path d="M5 12h14M12 5l7 7-7 7"></path>
-                            </svg>
-                        </a>
-                    </div>
-                    <div data-aos="zoom-in" class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Community Reviews and Ratings</h2>
-                        <p class="leading-relaxed text-base mb-4">Read and contribute genuine reviews to help others discover great food.</p>
-                        <a class="text-orange-500 inline-flex items-center">Learn More
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                <path d="M5 12h14M12 5l7 7-7 7"></path>
-                            </svg>
-                        </a>
-                    </div>
+                <div className="flex flex-wrap gap-6 justify-center">
+                    {serviceData.map((service, index) => (
+                        <ServiceItem key={index} title={service.title} description={service.description} icon={service.icon} color={service.color} />
+                    ))}
                 </div>
-                
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Services
+export default Services;
