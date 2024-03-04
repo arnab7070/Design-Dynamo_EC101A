ce970d30356bec1e4cbb910a69e98853fe100256
BIBHAB22 <bibhabindia@gmail.com>
2024-03-04 12:17:36 +0530

    Dialogflow chatbot added

--- yumYard/src/Components/FoodSection.jsx
+++ yumYard/src/Components/FoodSection.jsx
@@ -207,8 +207,8 @@ const FoodSection = () => {
 
   return (
     <section id="projects">
-      <h2 className="text-center text-5xl font-bold text-orange-500 mt-4 mb-8 md:mb-12 pt-60">
-        Our Menu
+      <h2 className="text-center text-5xl font-bold text-orange-500 mt-4 mb-8 md:mb-12 pt-10">
+        What's On your Mind
       </h2>
       <div className="text-black flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
