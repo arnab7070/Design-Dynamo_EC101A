4306c7bc8cfcaa0f209cad8dc6d465c399d1ddc1
BIBHAB22 <bibhabindia@gmail.com>
2024-03-09 00:18:12 +0530

    Tracking Page update

--- yumYard/src/App.jsx
+++ yumYard/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Home } from './Pages/Home';
 import { Login } from "./Pages/Login";
+import TrackingPage from "../src/Pages/Tracking";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="login" element={<Login />} />
+          <Route path="TrackingPage" element={<TrackingPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ yumYard/src/Pages/Tracking.jsx
@@ -0,0 +1,197 @@
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const TrackingPage = () => {
+  useEffect(() => {
+    Aos.init({
+      offset: 500,
+      duration: 1000,
+      easing: "ease-in",
+      delay: 200,
+    });
+  }, []);
+  return (
+    <div className="container mx-auto pl-10 pr-10 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
+        {/* Billing Address */}
+        <div
+          data-aos="flip-up"
+          className="bg-gray-100 shadow-md rounded-md p-6 mb-8"
+        >
+          <h2 className="text-xl font-semibold mb-4">Billing Address</h2>
+          <p className="text-gray-700">123 Main Street, Cityville, ABC</p>
+        </div>
+        {/* Payment Details */}
+        <div
+          data-aos="flip-up"
+          className="bg-gray-100 shadow-md rounded-md p-6 mb-8"
+        >
+          <h2 className="text-xl font-semibold mb-4">Payment Details</h2>
+          {/* Add payment details here */}
+        </div>
+        {/* Order Details */}
+        <div
+          data-aos="flip-up"
+          className="bg-gray-100 shadow-md rounded-md p-6 mb-8"
+        >
+          <h2 className="text-xl font-semibold mb-4">Order Details</h2>
+          <div className="flex justify-between">
+            <p className="text-gray-700">Product Name</p>
+            <p className="text-gray-700">$99.99</p>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-gray-700">Shipping</p>
+            <p className="text-gray-700">$9.99</p>
+          </div>
+          <hr className="my-2" />
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold">Total</p>
+            <p className="text-lg font-semibold">$109.98</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="space-y-8 ml-20 mr-20 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+        <h2 class="text-3xl text-black font-bold mb-12">Order Tracking</h2>
+        <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+          <div class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <svg
+              class="fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="10"
+            >
+              <path
+                fill-rule="nonzero"
+                d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+              />
+            </svg>
+          </div>
+          <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+            <div class="flex items-center justify-between space-x-2 mb-1">
+              <div class="font-bold text-slate-900">Order Placed</div>
+              <time class="font-caveat font-medium text-indigo-500">
+                08/06/2023
+              </time>
+            </div>
+            <div class="text-slate-500 ">
+              Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
+            </div>
+          </div>
+        </div>
+
+        <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+          <div class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <svg
+              class="fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="10"
+            >
+              <path
+                fill-rule="nonzero"
+                d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+              />
+            </svg>
+          </div>
+          <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+            <div class="flex items-center justify-between space-x-2 mb-1">
+              <div class="font-bold text-slate-900">Order Shipped</div>
+              <time class="font-caveat font-medium text-indigo-500">
+                09/06/2023
+              </time>
+            </div>
+            <div class="text-slate-500">
+              Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
+              morbi blandit cursus risus.
+            </div>
+          </div>
+        </div>
+
+        <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+          <div class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <svg
+              class="fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="10"
+            >
+              <path
+                fill-rule="nonzero"
+                d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+              />
+            </svg>
+          </div>
+          <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+            <div class="flex items-center justify-between space-x-2 mb-1">
+              <div class="font-bold text-slate-900">In Transit</div>
+              <time class="font-caveat font-medium text-indigo-500">
+                10/06/2023
+              </time>
+            </div>
+            <div class="text-slate-500">
+              Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
+              morbi blandit cursus risus.
+            </div>
+          </div>
+        </div>
+
+        <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+          <div class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <svg
+              class="fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="10"
+            >
+              <path
+                fill-rule="nonzero"
+                d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+              />
+            </svg>
+          </div>
+          <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+            <div class="flex items-center justify-between space-x-2 mb-1">
+              <div class="font-bold text-slate-900">Out of Delivery</div>
+              <time class="font-caveat font-medium text-indigo-500">
+                12/06/2023
+              </time>
+            </div>
+            <div class="text-slate-500">
+              Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
+              morbi blandit cursus risus.
+            </div>
+          </div>
+        </div>
+
+        <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group">
+          <div class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <svg
+              class="fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="12"
+            >
+              <path d="M12 10v2H7V8.496a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V12H0V4.496a.5.5 0 0 1 .206-.4l5.5-4a.5.5 0 0 1 .588 0l5.5 4a.5.5 0 0 1 .206.4V10Z" />
+            </svg>
+          </div>
+          <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+            <div class="flex items-center justify-between space-x-2 mb-1">
+              <div class="font-bold text-slate-900">Delivered</div>
+              <time class="font-caveat font-medium text-amber-500">
+                Exp. 12/08/2023
+              </time>
+            </div>
+            <div class="text-slate-500">
+              Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
+              morbi blandit cursus risus.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrackingPage;
