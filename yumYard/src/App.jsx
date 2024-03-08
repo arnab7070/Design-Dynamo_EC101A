3adedfb29b3289d7177845d6b83f04d5e99496b5
Supra707 <supratikde2@gmail.com>
2024-03-09 00:41:13 +0530

    fiux

--- yumYard/src/App.jsx
+++ yumYard/src/App.jsx
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
 import {BrowserRouter as  Router,Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Home } from "./Pages/Home";
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
 import { Home } from './Pages/Home';
 import { Login } from "./Pages/Login";
-import TrackingPage from "../src/Pages/Tracking";
->>>>>>> 4306c7bc8cfcaa0f209cad8dc6d465c399d1ddc1
-
 import SignUp from "./Components/SignUp";
-import Login from "./Components/login.jsx"
+import TrackingPage from "../src/Pages/Tracking";
 function App() {
   return (
-<<<<<<< HEAD
   <Router>
       <div className="App">
         {/* Place Home component inside a Route if it's meant to be a route */}
@@ -28,19 +19,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp/>} />
+        <Route path="TrackingPage" element={<TrackingPage />} />
       </Routes>
-    </Router>
-=======
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="TrackingPage" element={<TrackingPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
->>>>>>> 4306c7bc8cfcaa0f209cad8dc6d465c399d1ddc1
+  
+      </Router>
+ 
   );
 }
 
