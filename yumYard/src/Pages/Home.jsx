355db68fb50feb774b18e3cd9f56afc70537f82d
Arnab Nandi <arnabnandi7074@gmail.com>
2024-03-09 14:50:35 +0530

    panel updated

--- yumYard/src/Pages/Home.jsx
+++ yumYard/src/Pages/Home.jsx
@@ -26,7 +26,7 @@ export const Home = () => {
                 <Testimonials/>
                 <FooterComponent/>
                 <TestCart/>
-            </div>
+             </div>
         </div>
     )
 }
